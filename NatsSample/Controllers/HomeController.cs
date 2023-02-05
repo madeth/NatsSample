ee35c58ff20a3a25fcce73e86b7f72552b13abd5
madeth <madethg@gmail.com>
2023-02-05 23:27:42 +0900

    first commit

---
+++ NatsSample/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AlterNats;
+using System.Security.Cryptography;
+
+namespace NatsSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        static ValueTask<IDisposable>? _subscribeTask;
+        static int _counter = 1;
+
+        SemaphoreSlim Semaphore { get; set; } = new SemaphoreSlim(1, 1);
+        NatsKey NatsKey { get; set; } = new NatsKey("test.subject");
+        ILogger _logger;
+        INatsCommand _command;
+
+        public HomeController(INatsCommand command, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _command = command;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("OK");
+        }
+
+        [Route("/subscribe")]
+        public async Task<IActionResult> Subscribe()
+        {
+            Semaphore.Wait();
+            try
+            {
+                if (_subscribeTask is null)
+                {
+                    _subscribeTask = _command.SubscribeAsync(NatsKey, (string message) => OnReceive(message));
+                    _logger.LogDebug("Subscribed");
+                }
+
+            }
+            finally
+            {
+                Semaphore.Release();
+            }
+            await Task.Delay(1);
+
+            return Content("subscribe");
+        }
+
+        [Route("/publish")]
+        public async Task<IActionResult> Publish([FromQuery] string message = "message")
+        {
+            Semaphore.Wait();
+            try
+            {
+                await Task.Delay(1);
+                var _ = _command.PublishAsync(NatsKey, $"counter:{_counter}, date:{DateTime.Now.ToString("HH:mm:ss")}, {message}");
+                _counter++;
+            }
+            finally
+            {
+                Semaphore.Release();
+            }
+            return Content($"publish {message}");
+        }
+
+        [Route("/unsubscribe")]
+        public async Task<IActionResult> Unsubscribe()
+        {
+            await Task.Delay(1);
+            if (_subscribeTask?.IsCompleted ?? false)
+            {
+                var task = _subscribeTask?.AsTask();
+                if (task is not null)
+                {
+                    try
+                    {
+                        var subscribe = await task;
+                        subscribe.Dispose();
+                    }
+                    catch (NatsException e)
+                    {
+                        _logger.LogError(e, "Failed");
+                    }
+                    finally
+                    {
+                        _subscribeTask = null;
+                    }
+                    _logger.LogDebug("Unsubscribe");
+                }
+            }
+            return Content("unsubscribe");
+        }
+
+        private void OnReceive(string message)
+        {
+            _logger.LogInformation($"Received {message}");
+        }
+    }
+}
+
---
+++ NatsSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using AlterNats;
+using Serilog;
+using Serilog.Events;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvcCore();
+builder.Host.UseSerilog();
+
+var poolSize = 1;
+builder.Services.AddNats(
+    poolSize: poolSize,
+    configureOptions: (options) => options with
+    {
+        LoggerFactory = new MinimumConsoleLoggerFactory(LogLevel.Debug),
+        Url = "nats1:4222",
+        ConnectTimeout  = TimeSpan.FromSeconds(30),
+        RequestTimeout  = TimeSpan.FromSeconds(10),
+        ReconnectWait   = TimeSpan.FromSeconds(5),
+        ConnectOptions  = ConnectOptions.Default with { Verbose = true, Echo = true }
+    },
+    configureConnection: (connection) =>
+    {
+        connection.ReconnectFailed += (sender, e) => {
+            var date = DateTime.Now;
+            Log.Logger.Error($"ReconnectFailed: {e}");
+        };
+    }
+);
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+app.UseRouting();
+app.MapDefaultControllerRoute();
+app.Run();
